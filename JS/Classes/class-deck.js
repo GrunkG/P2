b7803b6563e19dfaa01328a164726d92c425e082
Andreas Andersen <vuferen@gmail.com>
2020-03-04 14:16:39 +0100

    Added deck class

---
+++ JS/Classes/class-deck.js
@@ -0,0 +1,23 @@
+//Needs Card class
+class Deck{
+    constructor(deck){
+        this.deck = deck;
+    }
+    static get getDeck(){
+        let deck = [];
+        for (let i = 0; i < Card.cardSuit.length; i++) {
+            for (let j = 0; j < Card.cardValue.length; j++) {
+                let card = new Card(Card.cardValue[j], Card.cardSuit[i], "default");
+                deck.push(card);
+            }
+        }
+        return deck;
+    }
+    addDeck(numberOfDecks){
+        for (let i = 0; i < numberOfDecks; i++) {
+            this.deck = this.deck.concat(Deck.getDeck);
+        }
+        return this.deck;
+    }
+    //Add shuffle method
+}
--- JS/test.js
+++ JS/test.js
@@ -2,10 +2,11 @@ let test = new Card("K", "H", "default");
 //test.printCard(200, 100, 200, document.body);
 //test.printCardFaceDown(100, 90, 40, document.body);
 
-for (let index = 0; index < 52; index++) {
-    let newCard = new Card(Card.cardValue[randMinMax(0,12)], Card.cardSuit[randMinMax(0,3)], "default");
-    newCard.printCard(randMinMax(80, 120), randMinMax(0, 1700), randMinMax(0, 800), document.body);
-}
+let newDeck = new Deck(Deck.getDeck);
+
+// for (let index = 0; index < newDeck.deck.length; index++) {
+//     newDeck.deck[index].printCard(100, 120 * index % 1560 + 100, 200 * Math.floor(index * 1/13 + 1) - 100, document.body);
+// }
 
 function randMinMax(min, max){
     return Math.floor(Math.random() * (max - min) + min);
