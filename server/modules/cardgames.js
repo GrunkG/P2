4f3721fbd9340327d3259663ff7884e668d83fe7
Nikolaj <qvintusdk@gmail.com>
2020-03-09 19:30:46 +0100

    Added card shuffle to cardgame class

--- server/modules/cardgames.js
+++ server/modules/cardgames.js
@@ -1,62 +1,100 @@
 class card {
-    constructor(type, value) {
-        this.type = type;
-        this.value = value; 
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
     }
 }
 
 class cardgame {
-    constructor(decks) {
-        super();
-        this.decks = generateDeck(decks);
+    constructor(decks, bJokers) {
+        this.jokers = bJokers; //Bool, true or false
+        this.decks = this.generateDeck(decks);
      }
      
-     generateSuit(type) {
-        let suit = [];
-        for (let card = 1; card <= 13; card++) {
-            if (card < 10) { 
-                suit.push(this.generateCard(type,card));
+    generateSuit(suit) {
+        let result = [];
+        let suitSize = this.jokers ? 15 : 13;
+        for (let card = 1; card <= suitSize; card++) {
+            if (card <= 10 && card > 1) { 
+                result.push(this.generateCard(suit,card));
                 continue;
             }
             switch (card) {
+                case 1:
+                    result.push(this.generateCard(suit,'Ace'));
+                    break;
                 case 11:
-                    suit.push(this.generateCard(type,'Jack'));
+                    result.push(this.generateCard(suit,'Jack'));
                     break;
                 case 12:
-                    suit.push(this.generateCard(type,'Queen'));
+                    result.push(this.generateCard(suit,'Queen'));
                     break;
                 case 13:
-                    suit.push(this.generateCard(type,'King'));
+                    result.push(this.generateCard(suit,'King'));
+                    break;
+                case 14:
+                case 15:
+                    result.push(this.generateCard(null,'Joker'));
                     break;
                 default: break;
             }
         }
-        return suit;
-     }
+        return result;
+    }
 
-     generateCard(type, value) {
+    generateCard(type, value) {
         let cardType = null;
         switch(type) {
             case 1: cardType = "Spades"; break;
             case 2: cardType = "Hearts"; break;
             case 3: cardType = "Diamonds"; break;
             case 4: cardType = "Clubs"; break;
-            default: break;
+            default: cardType = null; break;
         }        
         return new card(cardType, value);
-     }
+    }
 
-     generateDeck(num) { //Re-write as recursive function?
+    generateDeck(num) { //Re-write as recursive function?
         let decks = [];
         if (num < 1) return null;
         for (let deck = 1; deck <= num; deck++) {
             for (let suit = 1; suit <= 4; suit++) {
-                decks.push(this.generateSuit(suit));
+                decks =  decks.concat(this.generateSuit(suit));
             }
         }
         return decks;
-     }
+    }
 
+    drawCard() {
+        if (this.decks == null) return null;
+        return this.decks.shift();
+    }
+
+    putInDeck(card) {
+        if (card instanceof card) {
+            this.decks.push(card);
+            return true;
+        }
+        return false;
+    }
+
+    shuffleDeck() { //Using Fisher-Yates Shuffle - https://bost.ocks.org/mike/shuffle/
+        var m = this.decks.length, t, i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+      
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
+      
+          // And swap it with the current element.
+          t = this.decks[m];
+          this.decks[m] = this.decks[i];
+          this.decks[i] = t;
+        }
+      
+        return this.decks;
+    }
 }
 
 class player extends cardgame {
@@ -79,7 +117,7 @@ class player extends cardgame {
 
 }
 
-class blackjack extends cardgames {
+class blackjack extends cardgame {
     constructor(decks, sqlconnector) {
         super();
         this.sqlcon = sqlconnector;
@@ -145,6 +183,6 @@ class blackjack extends cardgames {
 }
 
 module.exports =  {
-    blackjack
+    cardgame
 };
 
