a2c6d9766f17d6e1c7ebda33e3f6189b91cd4a44
Vuferen <vuferen@gmail.com>
2020-04-27 20:36:42 +0200

    Big commit  - Login with encryption through express and passport modules       - uses the sql database       - does not use the old server       - the blackjack stuff is not on the express server       - you can test it by navigating to the file: "express-server-test.js" and then press f5 to run it and open localhost:3000 in your browser (plz work ;_;)

---
+++ express-server-test.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+
+const passport = require("passport");
+const initializePassport = require("./passport-config.js");
+initializePassport(passport);
+
+const PORT = 3000;
+
+const app = express(); //Creates express server
+
+//Lets the server use the session package
+app.use(session({ 
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended: true})); //Lets the server use bodyParser
+app.use(cookieParser()); //Lets the server use cookieParser
+app.use(flash()); //Lets the server use express-flash
+
+//Lets the server use passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Gives the client static files e.g. css and images
+app.use("/Style", express.static(__dirname + '/Style'));
+app.use("/Resources", express.static(__dirname + '/Resources'));
+app.use("/JS", express.static(__dirname + '/JS'));
+
+//Makes the server use ejs files from the views folder
+app.set("view-engine", "ejs"); 
+
+
+//---------- ROUTING ------------
+
+//Homepage
+app.get("/", isLoggedIn, (request, response) => {
+    response.render("blackjack.ejs");
+})
+//Other pages
+app.get("/login", isNotLoggedIn, (request, response) => {
+    response.render("login.ejs", {message:request.flash("loginMessage")});
+})
+app.get("/register", isNotLoggedIn, (request, response) => {
+    response.render("register.ejs", {message:request.flash("registerMessage")});
+})
+
+//Login handler
+app.post("/login", passport.authenticate("local-login", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true}), (request, response) => {
+        console.log(request.body.remember); //This part does not currently do anything
+        if (request.body.remember) { //Cookie to remember login
+            request.session.cookie.age = 30 * 24 * 60 * 60 * 1000; //Cookie max age of 30 days
+        } else {
+            request.session.cookie.expires = false; //Cookie expires at end of session
+        }
+        response.redirect("/");
+    }
+);
+
+//Register handler
+app.post("/register", passport.authenticate("local-register", {
+    successRedirect: "/",
+    failureRedirect: "/register",
+    failureFlash: true
+}));
+
+//Logout
+app.get("/logout", (request, response) => {
+    request.logout();
+    response.redirect("/login");
+})
+
+//---------- END OF ROUTING ------------
+
+//Redirects user that are not logged in to the login page
+function isLoggedIn(request, response, next) { 
+    if (request.isAuthenticated()) {
+        return next();
+    }
+    response.redirect("/login");
+}
+
+//Redirects user that are logged in to the home page
+function isNotLoggedIn(request, response, next) { 
+    if (request.isAuthenticated()) {
+        return response.redirect("/");
+    }
+    next();
+}
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ passport-config.js
@@ -0,0 +1,78 @@
+const LocalStrategy = require("passport-local").Strategy //Gets strategy class for passport
+
+//Database setup
+const mysql = require("mysql");
+const dbConfig = require("./server/dbConfig.js")
+const connection = mysql.createConnection(dbConfig);
+connection.query("USE P2");
+
+const bcrypt = require("bcrypt-nodejs"); //Used for cryptation
+
+function initialize(passport){
+    //A function passport needs for login
+    passport.serializeUser((user, done) => done(null, user.ID)); 
+
+    //A function passport needs for logout
+    passport.deserializeUser((id, done) => {
+        connection.query("SELECT * from account WHERE ID = ? ", [id], (error, rows) => {
+            done(error, rows[0]);
+        });
+    });
+
+    //Register
+    passport.use("local-register", new LocalStrategy({ 
+        usernameField: "username",
+        passwordField: "password",
+        passReqToCallback: true },
+        registerUser) //<--- This is the register function - See below
+    );
+
+    //Login
+    passport.use("local-login", new LocalStrategy({
+        usernameField: "username",
+        passwordField: "password",
+        passReqToCallback: true },
+        loginUser) //<--- This is the login function - See below
+    );
+}
+
+//Handles the registration process
+const registerUser = function(request, username, password, done) {
+    connection.query("SELECT * FROM account WHERE username = ? ", [username], (error, rows) => {
+        if (error) {
+            return done(error);
+        } else if (rows.length) { //rows.length == true when a username is taken
+            return done(null, false, request.flash("registerMessage", "Username already exists"));
+        } else { //Create new user as object
+            let newUser = {
+                username: username,
+                password: bcrypt.hashSync(password, null, null)
+            };
+
+            //Insert new user into database
+            let insertQuery = "INSERT INTO account (username, password) VALUES (?, ?)";
+            connection.query(insertQuery, [newUser.username, newUser.password], (error, rows) => {
+                newUser.ID = rows.insertId; //Add ID to the user object
+
+                return done(null, newUser);
+            });
+        }
+    });
+}
+
+//Handles the login process
+const loginUser = function loginUser(request, username, password, done) {
+    connection.query("SELECT * FROM account WHERE username = ? ", [username], (error, rows) => {
+        if (error) {
+            return done(error);
+        } else if (!rows.length) { //rows.length == false when a username does not exist in the database
+            return done(null, false, request.flash("loginMessage", "User not found"));
+        } else if (!bcrypt.compareSync(password, rows[0].password)){ //If the submitted password does not match the encrypted password
+            return done(null, false, request.flash("loginMessage", "Incorrect password"));
+        } else { //If the submitted password does match the encrypted password
+            return done(null, rows[0]);
+        }
+    });
+}
+
+module.exports = initialize;
