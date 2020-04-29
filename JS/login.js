bff01462718958ab8f3eeefb76216bf093a72119
Nikolaj <qvintusdk@gmail.com>
2020-04-29 12:25:18 +0200

    Login system done-ish

--- JS/login.js
+++ JS/login.js
@@ -21,17 +21,29 @@ function toggleLogout(){
 }
 
 function login(){
-    //Check if login is correct
+    //Check if login is correct -- Server
     //Log in the player
     //Hide login and show log out button
-    toggleLogin();
-    toggleLogout();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    websocket.send( JSON.stringify( {type: "loginsystem", content:"login", user: username, pass: password} ) );
+
+    //toggleLogin();
+    //toggleLogout();
 }
 
 function register(){
     //Check if username exists
     //Register the user
-    toggleRegister();
+
+    let username = document.getElementById("register__username").value;
+    let password = document.getElementById("register__password").value;
+
+    websocket.send( JSON.stringify( {type: "loginsystem", content:"register", user: username, pass: password} ) );
+
+    //toggleRegister();
 }
 
 function logout(){
@@ -39,3 +51,47 @@ function logout(){
     //Hide log out button and show log in button
     toggleLogout();
 }
+
+//Handle login response from server
+websocket.onmessage = (message) => { 
+
+    try {
+        let msg = JSON.parse(message.data);
+
+        if (msg.type == "login") {
+            switch(msg.state) {
+                case "success":
+                    //toggleLogin();
+                    initiateGame();
+                    toggleLogin();
+                    toggleLogout();
+                    break;
+                case "zeropassword":
+                    document.getElementById("login__container--error").innerHTML = "Password is empty, please provide password.";
+                    break;
+                case "noexist":
+                    document.getElementById("login__container--error").innerHTML = "Username or password is incorrect.";
+                    break;
+            }
+        } else if (msg.type == "register") {
+            //register__container--error
+            switch(msg.state) {
+                case "exists":
+                    document.getElementById("register__container--error").innerHTML = "User already exists, try another username.";
+                    break;
+                case "zeropassword":
+                    document.getElementById("register__container--error").innerHTML = "Password is empty, please provide password.";
+                    break;
+                case "success":
+                    document.getElementById("login__container--error").innerHTML = "User registered, please login.";
+                    toggleLogin();
+                    toggleRegister();
+                    break;
+            }
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+};
