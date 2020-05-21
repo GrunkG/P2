761d51d8010337466b1aef6422554251d46cbdbf
Nikolaj <qvintusdk@gmail.com>
2020-05-21 10:23:05 +0200

    Cleaned up login.js

--- JS/login.js
+++ JS/login.js
@@ -20,67 +20,44 @@ function toggleLogout(){
     toggleDisplayNone("logout-button", "grid");
 }
 
-function login(){
-    //Check if login is correct -- Server
-    //Log in the player
-    //Hide login and show log out button
-
+function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
     websocket.send( JSON.stringify( {type: "loginsystem", content:"login", user: username, pass: password} ) );
-
-    //toggleLogin();
-    //toggleLogout();
 }
 
-function register(){
-    //Check if username exists
-    //Register the user
-
+function register() {
     let username = document.getElementById("register__username").value;
     let password = document.getElementById("register__password").value;
     let checkbox = document.getElementById("register__checkbox");
+
     if (checkbox.checked) {
         websocket.send( JSON.stringify( {type: "loginsystem", content:"register", user: username, pass: password} ) );
     }
-
-    //toggleRegister();
 }
 
-function logout(){
-    //Log out the player
-    //Hide log out button and show log in button
+//Log out the player
+function logout() {
+    
+    /*Reset game platform, and prepare it for next login, 
+      should the player want to login again.*/
     resetGamePlatform();
     toggleLogout();
     toggleLogin();
+
+    //If the playagain button is shown, hide it.
     let playAgain = document.getElementById("player__play-again").style.display;
     if (playAgain != "none" && game != null)
         game.togglePlayAgain();
-        
+    
+    //Send the logout request to server.
     let username = document.getElementById("username").value;
-    websocket.send( JSON.stringify( {type: "loginsystem", content:"logout", user: username, secret: secret/* getCookie("secret") */} ) );
+    websocket.send( JSON.stringify( {type: "loginsystem", content:"logout", user: username, secret: secret} ) );
+    
+    //Prepare the login form.
     handleLoginsystem();
     clearForm();
-    //document.location.reload();
-}
-
-function keyHandling(e) {
-    e = e || window.event;
-    if (e.keyCode == '13') { //User hits ENTER
-        //Determine whether login or register is active
-        let login = document.getElementById("login").style.display;
-        let register = document.getElementById("register").style.display;
-
-        if (login == "grid" && register == "none") {
-            let button = document.getElementById("login").getElementsByTagName("input")[2];
-            button.click();
-        } else if (register == "grid" && login == "none") {
-            //Will change when 2nd password field is added [2] -> [3]
-            let button = document.getElementById("register").getElementsByTagName("input")[2]; 
-            button.click();
-        }
-    }
 }
 
 function clearForm() {
@@ -92,6 +69,32 @@ function clearForm() {
     document.getElementById("register__password").value = "";
 }
 
+/*Handling the document.onkeypress event
+    Current only handling the case of the user pressing enter
+*/
+function keyHandling(e) {
+    e = e || window.event;
+    if (e.keyCode == '13') { //User hits ENTER
+        //Determine whether login or register is active
+        let login = document.getElementById("login").style.display;
+        let register = document.getElementById("register").style.display;
+
+        if (login == "grid" && register == "none") {
+            let button = document.getElementById("login").getElementsByTagName("input")[2];
+            button.click();
+        } else if (register == "grid" && login == "none") {
+            let button = document.getElementById("register").getElementsByTagName("input")[2]; 
+            button.click();
+        }
+    }
+}
+
+/* void handleLoginsystem()
+    Handles initial client-server communication to the login system.
+        Upon login the different user statistics is set to the specific account, 
+        a random secret for that session is generated and should the user not have logged in 
+        for the day the daily bonus reward is shown.
+*/
 function handleLoginsystem() {
     websocket = new WebSocket(`ws://${host}:${port}/`);
     //Handle login response from server
@@ -103,8 +106,6 @@ function handleLoginsystem() {
             if (msg.type == "login") {
                 switch(msg.state) {
                     case "success":
-                        console.log("Was in login!");
-                        //toggleLogin();
                         document.getElementById("player__info--capital").innerHTML = msg.currency;
                         document.getElementById("player__info--wins").innerHTML = msg.games_won;
                         document.getElementById("player__info--losses").innerHTML = msg.games_lost;
@@ -131,7 +132,6 @@ function handleLoginsystem() {
                         break;
                 }
             } else if (msg.type == "register") {
-                //register__container--error
                 switch(msg.state) {
                     case "exists":
                         document.getElementById("register__container--error").innerHTML = "User already exists, try another username.";
